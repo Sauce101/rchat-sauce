ec2f379ce3c53bf310554f19b865179e5b6b9e7b
Michael Saucedo <michaelsaucedo443@gmail.com>
2020-02-09 00:02:03 -0800

    Update reg form duplicate username checker

--- wtform_fields.py
+++ wtform_fields.py
@@ -1,18 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+
+from models import User
 
 
 class RegistrationForm(FlaskForm):
     """ Registration form """
 
-    username = StringField('username_label', \
-        validators=[InputRequired(message="Username required"), \
-        Length(min=4, max=25, message="Username must be between 4 and 25")])
-    password = PasswordField('password_label', \
-        validators=[InputRequired(message="Password required"), \
-        Length(min=4, max=25, message="Password must be between 4 and 25")])
-    confirm_pswd = PasswordField('confirm_pswd_label', \
-        validators=[InputRequired(message="Password required"), \
-        EqualTo('password', message="Passwords must match")])
+    username = StringField('username_label', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25")])
+    confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a different username.")
